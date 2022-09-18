cadf2e50ab348fa54154fe2be071e6e3833e6b55
Nitin Varma <nitinvarma@pcast2.uclab.uc.edu>
2022-09-18 17:36:20 -0400

    Initial commit

---
+++ src/EvenOdd.java
@@ -0,0 +1,16 @@
+public class EvenOdd
+{
+    public static void main(String[] args)
+    {
+        int number = 2;
+        if(number%2==0)
+        {
+            System.out.println(number + " is Even");
+        }
+        else
+        {
+            System.out.println(number + " is Odd");
+        }
+    }
+
+}
